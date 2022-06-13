6a85923c5679dd4453d64ca7082890687173f6c3
Rafael Oliveira <83254653+RafaelLOliveira@users.noreply.github.com>
2022-06-13 15:53:47 -0300

    Update unit-test.js

--- test/unit-test.js
+++ test/unit-test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const message = require('../utils');
 describe('Message Test', () => {
  it('Welcome Message', () => {
-        assert.notStrictEqual(message.getWelcomeMessage(), outra mensagem);
+        assert.strictEqual(message.getWelcomeMessage(), outra mensagem);
     });
  it('Port Test', () => {
         assert.strictEqual(message.getPortMessage(), "Application Running on port");
